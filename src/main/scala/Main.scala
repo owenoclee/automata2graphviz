8de41918d015e4668958513b727f7daaf80a3de5
Owen Oclee <git@gru.bz>
2016-10-19 00:26:39 +0100

    Add Main

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,11 @@
+object Main {
+    def main(args: Array[String]): Unit = {
+        val argString = args.mkString
+        stringToFiniteAutomaton(argString)
+    }
+
+    def stringToFiniteAutomaton(str: String): FiniteAutomaton = {
+        //parse string and return resulting finite automaton
+        null
+    }
+}
