f42338ed085f1bfd5d03f067a72a98a29684156b
Owen Oclee <git@gru.bz>
2016-10-19 23:42:46 +0100

    Add missing alphabet sanity check

--- src/main/scala/FiniteAutomaton.scala
+++ src/main/scala/FiniteAutomaton.scala
@@ -9,7 +9,8 @@ class FiniteAutomaton(states: Set[String], finals: Set[String], alphabet: Set[Ch
                       transitions: Set[(String, Char, String)], initial: String) {
     //states that have at least one transition to/from
     val tStates = transitions.map{case (s1, _, _) => s1}.union(transitions.map{case (_, _, s2) => s2})
-
+    val tAlphabet = transitions.map{case (_, a, _) => a} //letters used by transitions
+    
     //sanity checking
     if(!states.contains(initial))
         throw InvalidFiniteAutomatonException("Initial state is not in the set of states")
@@ -17,6 +18,8 @@ class FiniteAutomaton(states: Set[String], finals: Set[String], alphabet: Set[Ch
         throw InvalidFiniteAutomatonException("Set of final states contains one or more unknown states")
     else if(states.union(tStates) != states)
         throw InvalidFiniteAutomatonException("Set of transitions contains one or more unknown states")
+    else if(alphabet.union(tAlphabet) != alphabet)
+        throw InvalidFiniteAutomatonException("Set of transitions contains a letter not in the alphabet")
 
     //map transitions to a sequence of dot statements
     var statements: Seq[Statement] = transitions.map{case (s1, c, s2) => s1 --> s2 :| ("label" := c.toString)}.toSeq
